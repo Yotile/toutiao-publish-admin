17d69d29f27d240cc58e5bebb54e53cc78f78932
Yotile <ismsg@outlook.com>
2021-05-27 11:31:10 +0800

    用户登录模块-创建登录组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// import Login from '../views/login'
+// 在Vuecli 创建的项目中 @ 表示src  它是 src 目录的路径别名
+// 好处: 它不受当前文件路径影响
+import Login from '@/views/login'
 
 Vue.use(VueRouter)
 
 // 路由配置表
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
 
 const router = new VueRouter({
   routes
