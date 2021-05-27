c730ad5c7a01eeb759c942af2e3910d1db69fa07
Yotile <ismsg@outlook.com>
2021-05-27 18:24:23 +0800

    用户登录模块-页面布局-实现基本登录功能-登录消息提示

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,124 @@
 <template>
-   <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!--
+      el-form 表单组件
+      每个表单项都必须使用 el-form-item 组件包裹
+     -->
+
+    <div class="login-form-wrap">
+      <div class="login-head">
+        <h2 class="logo">头条号管理平台</h2>
+      </div>
+      <el-form class="login-form" ref="form" :model="user">
+        <el-form-item>
+          <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked"
+            >我已阅读并同意用户协议和隐私条款</el-checkbox
+          >
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-btn" type="primary" @click="onLogin"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
+// 加载 request 模块
+import request from '@/utils/request'
+
 export default {
   name: 'LoginIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      },
+      checked: false // 是否同意协议的选中状态
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onLogin () {
+      // 获取表单数据(根据接口要求绑定数据)
+      const user = this.user
+
+      // 表单验证
+
+      // 验证通过,提交登录
+      request({
+        method: 'POST',
+        url: '/mp/v1_0/authorizations',
+        // data 用来设置 POST 请求体
+        data: user
+      }).then(res => {
+        // 登录成功
+        console.log(res)
+        this.$message({
+          message: '恭喜你，登录成功!',
+          type: 'success'
+        })
+      }).catch(err => {
+        // 登录失败
+        console.log('登录失败', err)
+        this.$message.error('登录失败，手机号或者验证码错误!')
+      })
+      // 处理后端响应结果
+      //   成功:xxx
+      //   失败:xxx
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+  .login-container {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: url('./login_bg.jpg') no-repeat;
+    background-size: cover;
+    .login-form-wrap {
+      min-width: 300px;
+      padding: 20px 50px 10px;
+      background-color: #fff;
+      .login-head {
+        display: flex;
+        justify-content: center;
+        .logo {
+          width: 200px;
+          height: 30px;
+          // background: url('./logo_index.png') no-repeat;
+          color:#409eff;
+          font-size: 26px;
+          text-align: center;
+        }
+      }
+      .login-form {
+        .login-btn {
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
