acea3e9bf6b78a5ad556633def5fdddd96620808
Yotile <ismsg@outlook.com>
2021-06-08 17:27:23 +0800

    内容管理页--ããã>基本页面布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,86 @@
 <template>
-  <div>内容管理</div>
+  <div class="article-container">
+    <el-card class="filter-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+          <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+          </el-breadcrumb>
+        <!-- 面包屑路径导航  end -->
+      </div>
+
+        <!-- 数据筛选表格 -->
+        <el-form ref="form" :model="form" label-width="60px" size="mini">
+          <el-form-item label="状态：">
+            <el-radio-group v-model="form.resource">
+              <el-radio label="全部"></el-radio>
+              <el-radio label="草稿"></el-radio>
+              <el-radio label="待审核"></el-radio>
+              <el-radio label="审核通过"></el-radio>
+              <el-radio label="审核失败"></el-radio>
+              <el-radio label="已删除"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="频道：">
+            <el-select v-model="form.region" placeholder="请选择频道">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="日期：">
+            <el-date-picker
+              v-model="form.data1"
+              type="datetimerange"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              :default-time="['12:00:00']">
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">筛选</el-button>
+          </el-form-item>
+        </el-form>
+        <!-- 数据筛选表格 end -->
+    </el-card>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        根据筛选条件共查询到 299 条结果：
+      </div>
+      <!-- 数据列表 -->
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        class="list-table"
+        stripe
+        size="mini"
+        >
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+      <!-- 数据列表 end -->
+
+    <!-- 数据分页 -->
+    <el-pagination
+      layout="prev, pager, next"
+      background
+      :total="1000">
+    </el-pagination>
+    <!-- 数据分页 end -->
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,14 +89,54 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.filter-card {
+  margin-bottom: 20px;
+}
+
+.list-table {
+  margin-bottom: 20px;
+}
+</style>
